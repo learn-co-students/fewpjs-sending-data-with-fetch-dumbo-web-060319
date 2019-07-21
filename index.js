a03b92b562a2a7f65999748487b894a18aff3c5e
Justin Hutsell <hutselljustin@gmail.com>
2019-07-21 14:33:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    let formData = {
+        name: `${name}`,
+        email: `${email}`
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => resp.json())
+        .then(json => idFinder(json.id))
+        .catch(function(error) {
+            console.log(error)
+            document.querySelector("body").append(error.message)
+        });
+
+
+
+
+
+    function idFinder(jsonId) {
+        document.querySelector("body").append(jsonId) 
+    }
+
+}
